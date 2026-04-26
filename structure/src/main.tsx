00c038a70ab1701232042b22dd9c9edb6a26b36d
yoohoony-gmail-com <yoohoony@gmail.com>
2026-04-26 22:05:20 +0900

    Add structure/src/main.tsx

---
+++ structure/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+} else {
+  throw new Error("Could not find root element with id 'root' to mount the application.");
+}
