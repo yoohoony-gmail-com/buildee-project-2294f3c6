f116a1807eb1f5d4a284000a1e93c5d41e0fc1e3
yoohoony-gmail-com <yoohoony@gmail.com>
2026-04-26 22:05:23 +0900

    Add tests/src/setupTests.ts

---
+++ tests/src/setupTests.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom/vitest';
+
+// This file is used to set up global configurations for tests.
+// By importing '@testing-library/jest-dom/vitest', we extend Vitest's `expect`
+// with helpful matchers for asserting on DOM nodes.
+// For example, you can use `expect(element).toBeInTheDocument()`.
+
+// This file should be referenced in the `setupFiles` option of your vitest.config.ts.
